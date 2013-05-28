5a147a9ee35688603cf2b0dc133c7e1ed99425ec
Bob Jansen <bob@firmhouse.com>
2013-05-28 14:44:45 +0200

    initia

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scouty::Application.config.secret_key_base = 'fde500d2f5bf08d07fd7fd1f469e4fe8d507e69849db9e7b6a2cfd738cc5d5e4885cb0f307eae5731cb9a20e6381cb79b74a4910d8a191a2fcc14bf5b59ccb73'
