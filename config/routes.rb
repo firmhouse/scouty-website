ce2efb265362a83d57263ca5aa0544efbacf4336
Bob Jansen <bob@firmhouse.com>
2013-05-29 14:57:36 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Scouty::Application.routes.draw do
+  get "promo/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root to: 'welcome#index'
+  root to: 'promo#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
