8f379e522b33e3f545fce2e6b028fc4e0eb6bf47
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-05-30 13:35:38 +0200

    Always add www from the root

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Scouty
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    require 'www_adder'
+    config.middleware.use "WwwAdder"
   end
 end
